e62c0ba6f694aea16696c95501d3e7202c9c4856
Ljubixonovich <vikvikvuk@gmail.com>
2019-11-12 11:30:50 +0100

    not found page upgraded

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { Btn } from '../../components/Btn/Btn';
 
 const NotFound = (props) => (
-   <h1>Page Not Found</h1>
+   <>
+      <h3 className='Center'>Page Not Found</h3>
+      <p className='Center'>Niste našli ono što tražite? Postavite pitanje za specijalno vašu situaciju</p>
+      <NavLink
+         to={{
+            pathname: 'contact',
+            aboutProps: {
+               hideContactInfo: true
+            }
+         }}
+      >
+         <Btn className='Center'>Pitaj</Btn>
+      </NavLink>
+   </>
 );
 
 export default NotFound;
